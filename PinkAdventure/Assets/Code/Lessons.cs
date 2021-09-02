acd1d90fc7d7c3cb03aa923ce31f7f03e309bc07
Valeria Vicious <59263802+ValeriaVicious@users.noreply.github.com>
2021-09-02 10:26:03 +0200

    Описание кдасса Lessons

---
+++ PinkAdventure/Assets/Code/Lessons.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+
+namespace PinkAdventure
+{
+    public class Lessons : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField] private Camera _mainCamera;
+        [SerializeField] private SpriteRenderer _back;
+
+        #endregion
+
+
+        #region UnityMethods
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+
+        }
+
+        private void OnDestroy()
+        {
+
+        }
+
+        #endregion
+    }
+}
+
