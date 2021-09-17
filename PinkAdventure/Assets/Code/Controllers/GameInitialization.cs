5d79b8556d045db4977843f543e97b86733b3a6c
Valeria Vicious <59263802+ValeriaVicious@users.noreply.github.com>
2021-09-17 12:57:49 +0200

    Создан и описан класс контроллеров Инициализация игры Поправка спрайт аниматора

---
+++ PinkAdventure/Assets/Code/Controllers/GameInitialization.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+namespace Adventure
+{
+    internal sealed class GameInitialization
+    {
+        public GameInitialization(CompositeControllers compositeControllers, GameConfig gameConfig)
+        {
+            Camera camera = Camera.main;
+            var playerIntialization = new PlayerInitialization(gameConfig.PlayerConfig.View);
+            var player = playerIntialization.GetLevelObject();
+
+            compositeControllers.Add(playerIntialization);
+            compositeControllers.Add(new PlayerAnimationController(gameConfig.PlayerConfig, player));
+            compositeControllers.Add(new PlayerController(gameConfig.PlayerConfig, player));
+        }
+    }
+}
